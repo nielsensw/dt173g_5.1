8083aee9e1360c98b311afff3b642a066b27f594
nielsensw <66014251+nielsensw@users.noreply.github.com>
2020-10-08 12:50:14 +0200

    Update courses.php
    updated code v2

--- courses.php
+++ courses.php
@@ -1,9 +1,9 @@
 <?php
 include("config/config.php");
 
-header("Content-Type: application/json; charset=UTF-8;");
+header("Content-Type: application/json;");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: X-Requested-With, Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization");
+header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE");
 
 // För att kunna använda PUT, POST, GET, DELETE
@@ -13,15 +13,17 @@ $course = new Course();
 // Datan som begärs eller skickas (arc)
 $data = json_decode(file_get_contents('php://input'), true);
 
+// KOllar efter id i url
+if(isset($_GET['id'])){
+    $index = $_GET['id'];
+}
+
 switch ($method) {
     case "GET":
-       
-        //kollar om resultatet (datan) är mer än 0/tom
-        //hämtar data
         $response = $course->getCourses();
+        //kollar om resultatet (datan) är mer än 0/tom
         if (sizeof($response) > 0) {
             http_response_code(200);
-            $response = array("message" => "Kurs tillagd");
         } else {
             http_response_code(404);
             $response = array("message" => "Inga kurser hittas");
@@ -33,7 +35,7 @@ switch ($method) {
         $name = $data['name'];
         $prog = $data['progression'];
         $syllabus = $data['syllabus'];
-        $index = $data['id'];
+       
         if ($course->updateCourse($code, $name, $prog, $syllabus, $index)) {
             http_response_code(200);
             $response = array("message" => "Course Updated");
@@ -44,31 +46,46 @@ switch ($method) {
         break;
     case "POST":
          //Lägger in värdena som givits i tex arc, till variabler som används som argument i metoder skapad i klassen
-        $code = $data['code'];
-        $name = $data['name'];
-        $prog = $data['progression'];
-        $syllabus = $data['syllabus'];
-        if ($course->addCourse($code, $name, $prog, $syllabus)) {
+    //    $code = $data['code'];
+      //  $name = $data['name'];
+    //    $prog = $data['progression'];
+      //  $syllabus = $data['syllabus'];
+      $code = $data['code'];
+      $name = $data['name'];
+      $progression = $data['progression'];
+      $syllabus = $data['syllabus'];
+        
+        
+        if ($course->addCourse($code, $name, $progression, $syllabus)) {
             http_response_code(201);
             $response = array("message" => "Course Created");
+              
+             
         } else {
             http_response_code(500);
             $response = array("message" => "nope");
         }
         break;
     case "DELETE":
+   
         // Raderar kurs (rad) i db beroende av angivet id
-        $index = $data['id'];
-        if ($course->remove($index)) {
+        if(!isset($index)){
+              http_response_code(501);
+            $response = array("message" => "No id found :/");
+        } else{
+            
+              if ($course->remove($index)) {
             http_response_code(200);
             $response = array("message" => "Course Deleted");
         } else {
             http_response_code(500);
             $response = array("message" => "Course was not deleted :/");
+         }
         }
+      
+      
         break;
 }
 // Skriver ut meddelande/felmeddelande
 echo json_encode($response);
-
 
